6fbed729037d889675985a2fe46dc8541b6953fc
Ryan Murphy <ryanmurphy@Ryans-MacBook-Pro-2.local>
2022-03-05 11:12:08 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,106 @@
+import React, { useEffect, useState, useMemo, useCallback } from "react";
+import { Pokemon, getAll, getByName } from "./API";
+
+import "./styles.css";
+
+
+interface PokemonWithPower extends Pokemon {
+  power: number
+}
+
+const calculatePower = (pokemon: Pokemon) =>
+  pokemon.hp +
+  pokemon.attack +
+  pokemon.defense +
+  pokemon.special_attack +
+  pokemon.special_defense +
+  pokemon.speed;
+
+
+const PokemonTable: React.FunctionComponent<{
+  pokemon: PokemonWithPower[];
+}> = ({ pokemon }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Type</td>
+          <td colSpan={6}>Stats</td>
+          <td>Power</td>
+        </tr>
+      </thead>
+      <tbody>
+        {pokemon.map((p) => (
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.name}</td>
+            <td>{p.type.join(",")}</td>
+            <td>{p.hp}</td>
+            <td>{p.attack}</td>
+            <td>{p.defense}</td>
+            <td>{p.special_attack}</td>
+            <td>{p.special_defense}</td>
+            <td>{p.speed}</td>
+            <td>{p.power}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+const MemoedPokemonTable = React.memo(PokemonTable);
+
+export default function App() {
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+  const [search, setSearch ] = useState("")
+
+  const onSetSearch = useCallback((evt) => {
+    setSearch(evt.target.value)
+  }, [])
+
+  useEffect(() => {
+    getByName(search).then(setPokemon)
+  }, [search])
+
+  const pokemonWithPower = useMemo(() => pokemon.map((p) => ({ ...p, power: calculatePower(p) })), [pokemon])
+
+  const [threshold, setThreshold] = useState(500);
+
+  const onSetThreshold = useCallback((evt) => {
+    const intValue = parseInt(evt.target.value, 10)
+    if (isNaN(intValue)) return
+
+    setThreshold(parseInt(evt.target.value, 10))
+  }, [])
+
+  const countOverThreshold = useMemo(() => pokemonWithPower.filter((p) => p.power > threshold).length, [pokemonWithPower, threshold])
+  const min = useMemo(() => Math.min(...pokemonWithPower.map(p => p.power)), [pokemonWithPower])
+  const max = useMemo(() => Math.max(...pokemonWithPower.map(p => p.power)), [pokemonWithPower])
+
+
+  return (
+    <div>
+      <div className="top-bar">
+
+        <div>Search</div>
+        <input type="text" value={search.toString()} onChange={onSetSearch}></input>
+
+        <div>Power threshold</div>
+        <input type="text" value={threshold.toString()} onChange={onSetThreshold} autoFocus style={{ cursor: 'pointer' }}></input>
+
+        <div>Count over threshold: {countOverThreshold}</div>
+
+      </div>
+      <div className="two-column">
+        <MemoedPokemonTable pokemon={pokemonWithPower} />
+        <div>
+          <div>Min: {min}</div>
+          <div>Max: {max}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
